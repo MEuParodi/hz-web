c30800dfaec9ca0b2c4a5199e11e3ed80ec816ab
Euge <meuparodi@gmail.com>
2023-02-23 21:56:52 -0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,10 @@
+"use strict";
+
+let nombre = document.querySelector("#name");
+let contact = document.querySelector("#contact");
+let email = document.querySelector("#email");
+document.querySelector("#email").addEventListener("click",SendMail);
+
+function SendMail(){
+    email.href = "mailto:contacto@haizea.com?subject=Consulta&body=Hola mi nombre es " + nombre.value + " y mi número de contacto es " + contact.value + ". El motivo de mi contacto es " ;
+}
