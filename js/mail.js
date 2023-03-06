df0bd7ed440ae60a311687172d22a9a55fb187c6
Euge <meuparodi@gmail.com>
2023-03-06 16:39:29 -0300

    agrego link google maps

--- js/mail.js
+++ js/mail.js
@@ -3,7 +3,7 @@
 let nombre = document.querySelector("#name");
 let contact = document.querySelector("#contact");
 let email = document.querySelector("#email");
-// document.querySelector("#email").addEventListener("click",SendMail);
+document.querySelector("#email").addEventListener("click",SendMail);
 
 function SendMail(){
     email.href = "mailto:contacto@haizea.com?subject=Consulta&body=Hola mi nombre es " + nombre.value + " y mi número de contacto es " + contact.value + ". El motivo de mi contacto es " ;
