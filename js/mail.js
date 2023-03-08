bcbf44a36351eba70d4e23c786f795092f54b3af
Euge <meuparodi@gmail.com>
2023-03-08 12:47:18 -0300

    cambios en form y método de envío

--- js/mail.js
+++ js/mail.js
@@ -2,9 +2,9 @@
 
 let nombre = document.querySelector("#name");
 let contact = document.querySelector("#contact");
-let email = document.querySelector("#email");
-document.querySelector("#email").addEventListener("click",SendMail);
+let sendEmail = document.querySelector("#sendEmail");
+document.querySelector("#sendEmail").addEventListener("click",SendMail);
 
 function SendMail(){
-    email.href = "mailto:contacto@haizea.com?subject=Consulta&body=Hola mi nombre es " + nombre.value + " y mi número de contacto es " + contact.value + ". El motivo de mi contacto es " ;
+    sendEmail.href = "mailto:contacto@haizea.com?subject=Consulta&body=Hola mi nombre es " + nombre.value + " y mi número de contacto es " + contact.value + ". El motivo de mi contacto es " ;
 }
